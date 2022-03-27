1426691222ca61d061e9c39a82b0f689753cbdbe
Vlad Eysmont <eysmont73@gmail.com>
2022-03-27 23:43:13 +0300

    Add Task_2

---
+++ Practice_2/Practice_2.Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_2.Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
