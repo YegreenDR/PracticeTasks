f27bb7ebc7798437ef18d18ea2f4e74b1f112abd
Vlad Eysmont <eysmont73@gmail.com>
2022-03-28 14:10:32 +0300

    Add Task_1

--- Practice_2/Task_1/Program.cs
+++ Practice_2/Task_1/Program.cs
@@ -4,9 +4,36 @@ namespace Practice_2.Task_1
 {
     internal class Program
     {
+
+        class A
+        {
+            public int a;
+            public int b;
+
+            public double FirstCalculate(int a, int b)
+            {
+                return (a + b) / 2;
+            }
+
+            public double SecondCalculate(int a, int b)
+            {
+                return (Math.Pow(b, 3) + Math.Sqrt(a));
+            }
+
+            public A(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+        }
+           
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            A a = new A(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Console.WriteLine($"Средне арифметическое = {a.FirstCalculate(a.a,a.b)}");
+            Console.WriteLine($"Подсчёт функции: {a.SecondCalculate(a.a,a.b)}");
         }
     }
 }
