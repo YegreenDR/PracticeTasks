272f8040b45940afaf49191f5d97f1cfb38bcafa
Vlad Eysmont <eysmont73@gmail.com>
2022-03-28 16:26:34 +0300

    Add Task_2

--- Practice_2/Practice_2.Task_2/Program.cs
+++ Practice_2/Practice_2.Task_2/Program.cs
@@ -2,11 +2,60 @@
 
 namespace Practice_2.Task_2
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+
+        class Organization
         {
-            Console.WriteLine("Hello World!");
+            private string _name = "";
+            private string _product = "";
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+            public string Product
+            {
+                get { return _product; }
+                set { _product = value; }
+            }
+            public void Print()
+            {
+                Console.WriteLine($"название организации:  {Name}, продукт организации:  {Product}");
+            }
+        }
+        class InsuranceCompany : Organization
+        {
+            public InsuranceCompany()
+            {
+                this.Name = "Страховая компания";
+                this.Product = "Страховка";
+            }
+        }
+        class OilAndGasCompany : Organization
+        {
+            public OilAndGasCompany()
+            {
+                this.Name = "Нефтегазовая компания";
+                this.Product = "Нефть и газ";
+            }
+        }
+        class Factory : Organization
+        {
+            public Factory()
+            {
+                this.Name = "завод";
+                this.Product = "салфетки";
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                InsuranceCompany insurance = new InsuranceCompany();
+                insurance.Print();
+                OilAndGasCompany oilGas = new OilAndGasCompany();
+                oilGas.Print();
+                Factory factory = new Factory();
+                factory.Print();
+            }
         }
     }
-}
