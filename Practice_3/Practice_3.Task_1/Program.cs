37f340aa59a617adecfc6d472ae3b21979dc9414
Vlad Eysmont <eysmont73@gmail.com>
2022-03-29 13:45:22 +0300

    Add task_1

---
+++ Practice_3/Practice_3.Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice_3.Task_1
+{
+    internal class Program
+    {
+
+        class Sravnenie
+        {
+            public static double cos(double a, double b)
+            {
+                return (a < b) ? a : b;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            double z = Math.Cos(x * 2) + Math.Cos(x - 3);
+            Console.WriteLine("z = " + z);
+            var v = Sravnenie.cos(z,x);
+            Console.WriteLine("Минимальный элемент: " +  v );
+        }
+    }
+}
