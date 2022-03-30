57c962c17199e98bc9f2ce80c2e4771276006f9f
Vlad Eysmont <eysmont73@gmail.com>
2022-03-30 15:15:22 +0300

    Add task_2

--- Practice_4/Practice_4.Task_2/Program.cs
+++ Practice_4/Practice_4.Task_2/Program.cs
@@ -4,9 +4,34 @@ namespace Practice_4.Task_2
 {
     internal class Program
     {
+        static double F(double x)
+        {
+            if ((x > -4) & (x < 1)) { return (3 * x) / (x - 2); }
+            else if (1 <= x) { return 3; }
+            else { throw new IndexOutOfRangeException("Ошибка: выход за диапазон значения X."); }
+        }
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Console.WriteLine("Введите х, где -4<x<1 или 1<=x:");
+                var x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("F(x)= " + F(x));
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
