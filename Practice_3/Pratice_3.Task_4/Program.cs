8bb3a4dbef816b9d49a5bbafca2e2f32af6bd458
Vlad Eysmont <eysmont73@gmail.com>
2022-03-30 13:13:04 +0300

    Add task_1

---
+++ Practice_3/Pratice_3.Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pratice_3.Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Practice_4/Practice_4.Task_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Practice_4.Task_1
+{
+    internal class Program
+    {
+
+        static double y1(double x)
+        {
+            return ((5 * x - 7) / (4 * x + 8));
+        }
+
+        static double y2(double x)
+        {
+            return ((Math.Tan(x)) + (Math.Cos(x)) / x - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите значение Х1: ");
+                double x1 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Результат формулы y1: " + y1(x1));
+                Console.WriteLine("Введите значение Х2: ");
+                double x2 = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Результат формулы y2: " + y2(x2));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message,ex.StackTrace);
+            }
+            Console.ReadLine();
+        }
+    }
+}
