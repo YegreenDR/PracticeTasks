6575e230f2a40a491c54aba11cb05daf09eb9142
Vlad Eysmont <eysmont73@gmail.com>
2022-03-31 15:44:17 +0300

    Add task_2

--- Practice_5/Practice_5.Task_2/Program.cs
+++ Practice_5/Practice_5.Task_2/Program.cs
@@ -6,7 +6,34 @@ namespace Practice_5.Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите размерность массива: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Random rnd = new Random();
+            int sum = 0;
+            int CountOtr = 0;
+            int CountZero = 0;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next(-10,10);
+                Console.Write("Массив: ");
+                Console.Write($"{arr[i]} ");
+                if (arr[i] > 0)
+                {
+                    sum += arr[i];
+                }
+                else if (arr[i] < 0)
+                {
+                    CountOtr++;
+                }
+                else if (arr[i] == 0)
+                {
+                    CountZero++;
+                }
+                Console.WriteLine($"Сумма всех положительных элементов: {sum}");
+                Console.WriteLine($"Кол-во отрицательных элементов: {CountZero}");
+                Console.WriteLine($"Кол-во нулевых элементов: {CountOtr}");
+            }
         }
     }
 }
