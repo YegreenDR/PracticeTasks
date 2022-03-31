1b96f3fc4d2c57d336ced9999094cbe35ed03586
Vlad Eysmont <eysmont73@gmail.com>
2022-03-31 16:47:57 +0300

    Add task_3

--- Practice_5/Practice_5.Task_3/Program.cs
+++ Practice_5/Practice_5.Task_3/Program.cs
@@ -6,7 +6,33 @@ namespace Practice_5.Task_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите размер квадратной матрицы: ");
+            int r = Convert.ToInt16(Console.ReadLine());
+            int[,] Matrix = new int[r, r];
+            Random random = new Random();
+            int rand;
+            Console.WriteLine("Введите диапазон чисел (2 числа через Enter): ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    rand = random.Next(a, b);
+                    Matrix[i, j] = rand;
+                }
+            }
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    Console.Write(Matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
