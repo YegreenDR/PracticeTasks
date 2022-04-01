fe008a5258b2c0b880df9864250d6bf34c9cb5e6
Vlad Eysmont <eysmont73@gmail.com>
2022-04-01 10:04:27 +0300

    Add task_5

---
+++ Practice_5/Practice_5.Task_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice_5.Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int Factorial(int n)
+            {
+                if (n == 1) return n * 1;
+
+                return n * Factorial(n - 1);
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("enter n as factorial");
+                int n = int.Parse(Console.ReadLine());
+                double f = Factorial(n - 1) / Factorial(n - 2);
+                Console.WriteLine($"f(n) = {f}");
+            }
+        }
+    }
+}
