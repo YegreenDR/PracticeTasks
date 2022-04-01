cf536693857b649cf5ab8db9e26af89ea5dd156f
Vlad Eysmont <eysmont73@gmail.com>
2022-04-01 09:06:00 +0300

    Add task_3

---
+++ Practice_5/Pracice_5.Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pracice_5.Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const n = 23;
+            const m = 40;
+
+        }
+    }
+}
--- Practice_5/Practice_5.Task_3/Program.cs
+++ Practice_5/Practice_5.Task_3/Program.cs
@@ -11,6 +11,7 @@ namespace Practice_5.Task_3
             int[,] Matrix = new int[r, r];
             Random random = new Random();
             int rand;
+            int count = 0;
             Console.WriteLine("Введите диапазон чисел (2 числа через Enter): ");
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
@@ -22,16 +23,21 @@ namespace Practice_5.Task_3
                     Matrix[i, j] = rand;
                 }
             }
-
+            Console.WriteLine("Введите число поиска меньшего значения: ");
+            int d = int.Parse(Console.ReadLine());
             for (int i = 0; i < r; i++)
             {
                 for (int j = 0; j < r; j++)
                 {
+                    if (Matrix[i, j] > d)
+                    {
+                        count++;
+                    }
                     Console.Write(Matrix[i, j] + "\t");
                 }
                 Console.WriteLine();
             }
-
+            Console.WriteLine("Кол-во чисел меньше указанного: ", count);
             Console.ReadKey();
         }
     }
