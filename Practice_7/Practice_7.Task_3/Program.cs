f2992a6bfaa3ec3e558c91e04c5ad66f91298206
Vlad Eysmont <eysmont73@gmail.com>
2022-04-01 23:06:13 +0300

    Add task_3

---
+++ Practice_7/Practice_7.Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Practice_7.Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            Regex regex = new Regex(@"[0-9](\d*)");
+            MatchCollection matches = regex.Matches(str);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine(match.Value);
+            }
+            else
+            {
+                Console.WriteLine("Совпадений не найдено");
+            }
+        }
+    }
+}
