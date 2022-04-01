3a47f1d550e8b9b4ecba2c0fbf096f7fffd72576
Vlad Eysmont <eysmont73@gmail.com>
2022-04-01 22:50:54 +0300

    Add task_2

---
+++ Practice_7/Practice_7.Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Practice_7.Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string str = Console.ReadLine();
+            Regex regex = new Regex(@"(<html>|<form>|<h1>)(\w*)");
+            MatchCollection matches = regex.Matches(str);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches)
+                    Console.WriteLine("Строка содержит код HTML");
+            }
+            else
+            {
+                Console.WriteLine("Строка не содержит код HTML");
+            }
+        }
+    }
+}
