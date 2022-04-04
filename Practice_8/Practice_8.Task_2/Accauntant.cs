ab96016ad801be97933cae127aa9339f1007d0dc
Vlad Eysmont <eysmont73@gmail.com>
2022-04-04 17:48:44 +0300

    Add task_2

--- Practice_8/Practice_8.Task_2/Accauntant.cs
+++ Practice_8/Practice_8.Task_2/Accauntant.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace Practice_8.Task_2
 {
+    internal class Accauntant
+    {
 
-        class Accauntant
+    public bool AskForBonus(Dolznosti dol, int hours)
         {
-        public bool AskForBonus(Dolzhnosti dol, int hours)
-            {
-                if (hours > (int)dol) return true;
-                else return false;
-            }
+            if (hours > (int)dol) return true;
+            else return false;
         }
     }
+}
--- Practice_8/Practice_8.Task_2/Program.cs
+++ Practice_8/Practice_8.Task_2/Program.cs
@@ -1,60 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice_8.Task_2
 {
+
+    enum Dolznosti
+    {
+        Менеджер = 120,
+        Стажер = 45,
+        Кассир = 90,
+    }
     internal class Program
     {
-        enum Dolzhnosti
-        {
-            Управляющий = 90,
-            Менеджер = 100,
-            Пиццамейкер = 120,
-            Кассир = 120,
-            Стажер = 50,
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine(" 1) Управляющий");
-            Console.WriteLine(" 2) Менеджер");
-            Console.WriteLine(" 3) Кассир");
-            Console.WriteLine(" 4) Стажер");
+            Console.WriteLine("ДОЛЖНОСТИ");
+            Console.WriteLine();
+            Console.WriteLine(" 1. Менеджер");
+            Console.WriteLine(" 2. Кассир");
+            Console.WriteLine(" 3. Стажер");
             Console.WriteLine();
             Console.Write("Выберите должность: ");
             int a = Convert.ToInt16(Console.ReadLine());
-            Dolzhnosti worker = new Dolzhnosti();
+            Dolznosti worker = new Dolznosti();
             switch (a)
             {
                 case 1:
                     {
                         Console.Write("Количество отработанных часов для получения премии: ");
-                        Console.WriteLine(((int)Dolzhnosti.Управляющий)); worker = Dolzhnosti.Управляющий;
+                        Console.WriteLine((int)Dolznosti.Менеджер); worker = Dolznosti.Менеджер;
                     }
                     break;
                 case 2:
                     {
                         Console.Write("Количество отработанных часов для получения премии: ");
-                        Console.WriteLine((int)Dolzhnosti.Менеджер); worker = Dolzhnosti.Менеджер;
+                        Console.WriteLine((int)Dolznosti.Кассир); worker = Dolznosti.Кассир;
                     }
                     break;
                 case 3:
                     {
                         Console.Write("Количество отработанных часов для получения премии: ");
-                        Console.WriteLine((int)Dolzhnosti.Кассир); worker = Dolzhnosti.Кассир;
-                    }
-                    break;
-                case 4:
-                    {
-                        Console.Write("Количество отработанных часов для получения премии: ");
-                        Console.WriteLine((int)Dolzhnosti.Стажер); worker = Dolzhnosti.Стажер;
+                        Console.WriteLine((int)Dolznosti.Стажер); worker = Dolznosti.Стажер;
                     }
                     break;
                 default: Console.WriteLine("Введённое число не опознано."); break;
             }
-
             Console.WriteLine();
             Console.WriteLine("Введите количество отработанных часов за месяц: ");
             int h = Convert.ToInt16(Console.ReadLine());
