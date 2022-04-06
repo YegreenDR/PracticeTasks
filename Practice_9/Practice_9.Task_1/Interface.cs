a4a7ab077f3f1f96a130c99abfe3f754b3cc17ab
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-06 15:04:58 +0300

    Add task1

--- Practice_9/Practice_9.Task_1/Interface.cs
+++ Practice_9/Practice_9.Task_1/Interface.cs
@@ -1,28 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practice_9.Task_1
+﻿namespace task1
 {
-    internal class Interface
+    interface Ix
     {
-        interface Ix
-        {
-            void IxF0(string xkey);
-            void IxF1();
-        }
-        interface Iy
-        {
-            void F0(string ykey);
-            void F1();
-        }
-        interface Iz
-        {
-            void F0(string zkey);
-            void F1();
-        }
-
+        void IxF0(string xkey);
+        void IxF1();
+    }
+    interface Iy
+    {
+        void F0(string ykey);
+        void F1();
+    }
+    interface Iz
+    {
+        void F0(string zkey);
+        void F1();
     }
 }
