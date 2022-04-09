08d92e158b1d45640658af913efc85430b12615e
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-09 10:38:57 +0300

    Add task_3

---
+++ Practice_12/Practice_12.Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿public delegate string Stroka(string a);
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите действие: \n1 Перено строки после запятой \n2 Удалить первое слово  \n3 Замена точек на запятые.");
+        string a = Console.ReadLine();
+        Console.WriteLine("Введите строку: ");
+        string x = Console.ReadLine();
+        switch (a)
+        {
+            case "1":
+                Stroka calc1 = (c) => c;
+                string del1 = x.Replace(",", "\n");
+                Console.WriteLine("Ответ:{0}", del1);
+                break;
+            case "2":
+                Stroka calc = (c) => c;
+                string del = x.Remove(0, x.IndexOf(' ') + 1);
+                Console.WriteLine("Ответ:{0}", del);
+                break;
+            case "3":
+                Stroka calc2 = (c) => c;
+                string zamena = x.Replace(".", ",");
+                Console.WriteLine("Ответ:{0}", zamena);
+                break;
+        }
+        Console.ReadKey();
+    }
+}
