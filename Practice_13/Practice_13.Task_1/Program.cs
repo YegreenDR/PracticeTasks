ea6f58dcdaa5ff6eb2e28aa8107ae4d49eecb312
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-09 17:55:06 +0300

    Add task_1

---
+++ Practice_13/Practice_13.Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public delegate void NoParameters();
+
+    static void Main()
+    {
+        NoParameters noprm = new(ShowMessage1);
+        noprm += ShowMessage2;
+        noprm += ShowMessage3;
+        noprm();
+        Console.ReadLine();
+    }
+    private static void ShowMessage1() => Console.WriteLine("Первое сообщение");
+    private static void ShowMessage2() => Console.WriteLine("Второе сообщение");
+    private static void ShowMessage3() => Console.WriteLine("Третье сообщение");
+}
