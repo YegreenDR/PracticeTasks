765223417e971e980bfe6e9be297ea7f64df79b1
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-10 20:57:37 +0300

    Add task_3

---
+++ Practice_13/Practice_13.Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿delegate void MyInfoStateHandler(string message);
+class MyInfo
+{
+    string _name;
+    public event MyInfoStateHandler Adding;
+    public event MyInfoStateHandler Added;
+    public MyInfo(string name)
+    {
+        _name = name;
+    }
+    public void AddLetter(string name)
+    {
+        Adding($"Будет добавлена буква а: {name}");
+        name += "a";
+        Added($"Добавление буквы а: {name}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyInfo info = new MyInfo("Владислав");
+        info.Adding += Display;
+        info.Added += Display;
+        info.AddLetter("Владислав");
+    }
+    static void Display(string message) => Console.WriteLine(message);
+}
+
