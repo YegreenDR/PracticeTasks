02e908df6d7e61e595f35d576d936dc1fef3f787
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-11 12:03:56 +0300

    Add task_3

---
+++ Practice_14/Practice_14.Task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void FirstThread(int a)
+    {
+        double sum = 0;
+        for (int i = 1; i < 10; i++)
+        {
+            sum = sum + Math.Pow(a, i);
+            Console.WriteLine($"Первый метод: {sum}");
+        }
+    }
+    static void SecondThread(double a)
+    {
+        double multiply = 1;
+        for (int j = 1; j < 10; j++)
+        {
+            multiply = multiply * Math.Pow(a, j);
+            Console.WriteLine($"Второй метод: {multiply}");
+        }
+    }
+    static void Main(string[] args)
+    {
+        int a;
+        Console.Write("Введите А: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        int n;
+        Console.Write("Введите N: ");
+        n = Convert.ToInt32(Console.ReadLine());
+        Thread thread1 = new Thread(() => FirstThread(a));
+        Thread thread2 = new Thread(() => SecondThread(n));
+        Thread thread3 = new Thread(() => FirstThread(a));
+        thread1.Start();
+        thread2.Start();
+        thread3.Start();
+        Console.ReadLine();
+    }
+}
