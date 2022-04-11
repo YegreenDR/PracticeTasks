f527f2ab8167132dccbe5e230f131583a62e3905
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-11 10:49:37 +0300

    Add task_2

---
+++ Practice_14/Practice_14.Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+static void mythread1()
+{
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += i;
+        Console.WriteLine("Поток 1 выводит " + sum);
+    }
+    
+}
+
+static void mythread2()
+{
+    int sum1 = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum1 += i;
+        Console.WriteLine("Поток 2 выводит " + sum1);
+    }
+}
+
+Stopwatch sWatch = new Stopwatch();
+Thread thread1 = new Thread(mythread1);
+Thread thread2 = new Thread(mythread2);
+thread1.Priority = ThreadPriority.Highest;
+thread2.Priority = ThreadPriority.AboveNormal;
+
+sWatch.Start();
+thread1.Start();
+Thread.Sleep(1000);
+sWatch.Stop();
+thread2.Start();
+
+Console.WriteLine("Время затраченное на первый поток (в мс.) = "+sWatch.ElapsedMilliseconds.ToString());
+Console.ReadLine();
+
+
