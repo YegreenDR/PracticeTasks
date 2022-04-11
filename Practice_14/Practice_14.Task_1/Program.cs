8be55926bb70092eba9a52e02d0ba052c5fe8051
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-11 10:00:37 +0300

    Add task_1

---
+++ Practice_14/Practice_14.Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿static void mythread1()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Поток 1 выводит " + i);
+    }
+}
+
+static void mythread2()
+{
+
+    for (int i = 10; i < 20; i++)
+    {
+        Console.WriteLine("Поток 2 выводит " + i);
+    }
+}
+
+static void mythread3()
+{
+    for (int i = 20; i < 30; i++)
+    {
+        Console.WriteLine("Поток 3 выводит " + i);
+    }
+}
+
+    Thread thread1 = new Thread(mythread1);
+    Thread thread2 = new Thread(mythread2);
+    Thread thread3 = new Thread(mythread3);
+    thread1.Priority = ThreadPriority.Highest;
+    thread2.Priority = ThreadPriority.AboveNormal;
+    thread3.Priority = ThreadPriority.Lowest;
+
+    thread1.Start();
+    Thread.Sleep(1000);
+    thread2.Start();
+    Thread.Sleep(1000);
+    thread3.Start();
+
+    Console.ReadLine();
+
+
