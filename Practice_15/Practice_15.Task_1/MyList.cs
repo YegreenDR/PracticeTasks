ce068529192141f338b36a1114819f8a33ce3ff3
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-13 13:11:03 +0300

    Add task_1

---
+++ Practice_15/Practice_15.Task_1/MyList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace Practice_15.Task_1
+{
+    class MyList<T> : IEnumerable<T>
+    {
+        public int count { get { return array.Length; } }
+        T[] array = new T[0];
+        public void Add(T item)
+        {
+
+            Array.Resize(ref array, array.Length + 1);
+
+            array[array.Length - 1] = item;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return array.GetEnumerator();
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+    }
+}
---
+++ Practice_15/Practice_15.Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Practice_15.Task_1;
+    static void Main(string[] args)
+    {
+        MyList<int> myList = new MyList<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            myList.Add(i);
+        }
+        foreach (var item in myList)
+        {
+            Console.Write($"{item},");
+        }
+        var count = myList.count;
+        Console.WriteLine($"\nКоличество элементов: {count} \n3й элемент: {myList[2]}");
+        Console.ReadLine();
+    }
