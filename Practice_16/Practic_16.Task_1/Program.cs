b889e7cdc754c941473372e0f2d6c88993a7b1e8
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-14 13:03:27 +0300

    Add task_1

---
+++ Practice_16/Practic_16.Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+            Random rand = new Random();
+            Console.WriteLine("Сколько цифр должно быть в исходном файле (f)?");
+            int n = int.Parse(Console.ReadLine());
+            int r;
+
+            File.WriteAllText("D:\\f.txt", "");
+            File.WriteAllText("D:\\g.txt", "");
+            for (int i = 0; i < n; i++)
+            {
+                r = rand.Next(0, 100);
+                File.AppendAllText("D:\\f.txt", r + " ");
+            }
+
+            FileStream f = new FileStream("D:\\f.txt", FileMode.Open
+            , FileAccess.Read);
+            FileStream g = new FileStream("D:\\g.txt", FileMode.Open
+            , FileAccess.Write);
+            StreamReader rf = new StreamReader(f, Encoding.UTF8);
+            StreamWriter wg = new StreamWriter(g, Encoding.UTF8);
+
+            string str = File.ReadAllText("D:\\f.txt");
+            int[] arr = str
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(m => int.Parse(m))
+                .ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] % 3 == 0)
+                {
+                    string z = arr[i] + " ";
+                    wg.Write(z);
+                }
+            }
+
+            wg.Close();
+        
