f1c2a1532bf7272fc53ff27841cba47c862aa374
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-15 13:22:49 +0300

    Add task_2

---
+++ Practice_17/GameLib/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace GameLib
+{
+    public class Game
+    {
+        private string Name;
+        private string Genre;
+        public Game(string name, string genre)
+        {
+            Genre = genre;
+            Name = name;
+        }
+        public void Info(Action<string> action)
+        {
+            action($"Game - {this.Name} in genre - {this.Genre}");
+        }
+
+    }
+}
---
+++ Practice_17/Practice_17.Task_2/Program.cs
@@ -0,0 +1,3 @@
+﻿using GameLib;
+Game game = new Game("Witcher 3: Wild Hunt", " action, fantasy");
+game.Info(Console.WriteLine);
