11009d8f64f49cf5ed2e752a646211e8e9a4b720
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-15 13:09:26 +0300

    Add task_1

---
+++ Practice_17/Practice_17.Task_(1)/Program.cs
@@ -0,0 +1,14 @@
+﻿using MyLib;
+Console.WriteLine("Введите стороны треугольника:");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());  
+Triangle triag = new Triangle();
+Console.WriteLine("Периметр треугльника равен " + triag.TriPerimetr(a,b,c));
+Console.WriteLine(triag.TriangleForm(a,b,c));
+Console.WriteLine("Введите стороны прямоугольника:");
+int a1 = int.Parse(Console.ReadLine());
+int b1 = int.Parse(Console.ReadLine());
+Rectangle rec = new Rectangle();
+Console.WriteLine(rec.RecPerimetr(a1,b1));
+Console.WriteLine(rec.RecSquare(a1,b1));
