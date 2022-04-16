0ba832e1bc821bad980dcf3e1ef216f9182307f7
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-16 17:40:31 +0300

    Add task_2

--- Practice_18/Practice_18.Task_2/Program.cs
+++ Practice_18/Practice_18.Task_2/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-
-namespace ConsoleApplication1
+﻿namespace Practice_18.Task_2
 {
     class Program
     {
