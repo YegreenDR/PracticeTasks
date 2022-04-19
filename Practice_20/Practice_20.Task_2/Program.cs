0e0f49bf5d558154cadccfee66d85818f1eb4985
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-19 11:34:32 +0300

    Add task_2

--- Practice_20/Practice_20.Task_2/Program.cs
+++ Practice_20/Practice_20.Task_2/Program.cs
@@ -5,7 +5,7 @@ Task task1 = new Task(() =>
     global::System.Console.WriteLine(z1);
 });
 Task task2 = new Task(() =>
-{
+{   
     double z2 = 2 * Math.Tan(a) + 2 * Math.Sin(a);
     global::System.Console.WriteLine(z2);
 });
--- Practice_20/Practice_20.Task_3/Program.cs
+++ Practice_20/Practice_20.Task_3/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.Write("Введите число:");
+string str = Console.ReadLine();
+for (int i = str.Length - 1; i >= 0; i--) Console.Write(str[i]);
+Console.WriteLine();
