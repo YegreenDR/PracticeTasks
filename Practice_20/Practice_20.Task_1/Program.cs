47fa30934875355fa6e71fb0b950e635b1ed1dd7
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-19 10:26:55 +0300

    Add task_1

--- Practice_20/Practice_20.Task_1/Program.cs
+++ Practice_20/Practice_20.Task_1/Program.cs
@@ -1,9 +1,8 @@
-﻿Task task1 = new Task(() =>
+﻿Task task1 = Task.Run(() =>
 {
     Console.Write("Введите число:");
     string str = Console.ReadLine();
     for (int i = str.Length - 1; i >= 0; i--) Console.Write(str[i]);
     Console.WriteLine();
 });
-task1.Start();
 task1.Wait();
