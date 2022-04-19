8c1913b259913c6de775fc22ad7771d5847ae6aa
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-19 12:17:48 +0300

    Add task_1

--- Practice_21/Practice_21.Task_1/Program.cs
+++ Practice_21/Practice_21.Task_1/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        Water water = new Water(WaterState.LIQUID);
+        Water water = new Water(new LiquidWaterState());
         water.Heat();
         water.Frost();
         water.Frost();
@@ -10,49 +10,68 @@
         Console.Read();
     }
 }
-enum WaterState
-{
-    SOLID,
-    LIQUID,
-    GAS
-}
 class Water
 {
-    public WaterState State { get; set; }
+    public IWaterState State { get; set; }
 
-    public Water(WaterState ws)
+    public Water(IWaterState ws)
     {
         State = ws;
     }
 
     public void Heat()
     {
-        if (State == WaterState.SOLID)
-        {
-            Console.WriteLine("Превращаем лед в жидкость");
-            State = WaterState.LIQUID;
-        }
-        else if (State == WaterState.LIQUID)
-        {
-            Console.WriteLine("Превращаем жидкость в пар");
-            State = WaterState.GAS;
-        }
-        else if (State == WaterState.GAS)
-        {
-            Console.WriteLine("Повышаем температуру водяного пара");
-        }
+        State.Heat(this);
     }
     public void Frost()
     {
-        if (State == WaterState.LIQUID)
-        {
-            Console.WriteLine("Превращаем жидкость в лед");
-            State = WaterState.SOLID;
-        }
-        else if (State == WaterState.GAS)
-        {
-            Console.WriteLine("Превращаем водяной пар в жидкость");
-            State = WaterState.LIQUID;
-        }
+        State.Frost(this);
+    }
+}
+
+interface IWaterState
+{
+    void Heat(Water water);
+    void Frost(Water water);
+}
+
+class SolidWaterState : IWaterState
+{
+    public void Heat(Water water)
+    {
+        Console.WriteLine("Превращаем лед в жидкость");
+        water.State = new LiquidWaterState();
+    }
+
+    public void Frost(Water water)
+    {
+        Console.WriteLine("Продолжаем заморозку льда");
+    }
+}
+class LiquidWaterState : IWaterState
+{
+    public void Heat(Water water)
+    {
+        Console.WriteLine("Превращаем жидкость в пар");
+        water.State = new GasWaterState();
+    }
+
+    public void Frost(Water water)
+    {
+        Console.WriteLine("Превращаем жидкость в лед");
+        water.State = new SolidWaterState();
+    }
+}
+class GasWaterState : IWaterState
+{
+    public void Heat(Water water)
+    {
+        Console.WriteLine("Повышаем температуру водяного пара");
+    }
+
+    public void Frost(Water water)
+    {
+        Console.WriteLine("Превращаем водяной пар в жидкость");
+        water.State = new LiquidWaterState();
     }
 }
