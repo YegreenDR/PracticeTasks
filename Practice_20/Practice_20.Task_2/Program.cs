ce9a3ecabcaa087882dee57f2d304c2cbf9dec73
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-19 10:15:09 +0300

    Add task_2

---
+++ Practice_20/Practice_20.Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿double a = 2;
+Task task1 = new Task(() =>
+{
+    double z1 = (Math.Cos(a) + Math.Sin(a)) / (Math.Cos(a) - Math.Sin(a));
+    global::System.Console.WriteLine(z1);
+});
+Task task2 = new Task(() =>
+{
+    double z2 = 2 * Math.Tan(a) + 2 * Math.Sin(a);
+    global::System.Console.WriteLine(z2);
+});
+task1.Start();
+task2.Start();
+task1.Wait();
+task2.Wait();
