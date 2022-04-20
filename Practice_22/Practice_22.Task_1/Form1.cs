b4f99a5b79e10c0df2c7f2a1d01dd81447059d46
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-20 09:12:01 +0300

    Add task_1

---
+++ Practice_22/Practice_22.Task_1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_22.Task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox2.Text);
+            double b = Convert.ToDouble(textBox3.Text);
+            double u = (a * a + b * b + 2);
+            textBox1.Text = u.ToString();
+        }
+    }
+}
