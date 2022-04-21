48436110d824f507a8564c9141f42682220cdfb2
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-21 12:11:18 +0300

    Add task_4

---
+++ Practice_23/Practice_23.Task_4/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_23.Task_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox2.Text);
+            int b = Convert.ToInt32(textBox3.Text);
+            if (textBox1.Text == "К" | textBox1.Text == "к")
+            {
+                Button butt = new Button();
+                butt.Location = new Point(a, b);
+                this.Controls.Add(butt);
+                butt.MouseMove += ctrButton_MouseMove;
+            }
+            if (textBox1.Text == "м" | textBox1.Text == "М")
+            {
+                Label lab=  new Label();
+                lab.Location = new Point(a, b);
+                lab.Text = "Метка";
+                this.Controls.Add(lab);
+                lab.MouseMove += ctrLabel_MouseMove;
+            }
+            if (textBox1.Text == "П" | textBox1.Text == "п")
+            {
+                TextBox textb = new TextBox();
+                textb.Location = new Point(a, b);
+                this.Controls.Add(textb);
+                textb.MouseMove += ctrTextbox_MouseMove;
+            }
+        }
+        private void ctrButton_MouseMove(Object sender, MouseEventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Dispose();
+        }
+        private void ctrTextbox_MouseMove(Object sender, MouseEventArgs e)
+        {
+            TextBox t = (TextBox)sender;
+            t.Dispose();
+        }
+        public void ctrLabel_MouseMove(Object sender, MouseEventArgs e)
+        {
+            Label l = (Label)sender;
+            l.Dispose();
+        }
+    }
+}
