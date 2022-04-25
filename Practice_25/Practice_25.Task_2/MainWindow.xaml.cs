6e3aecbaaeccc6546e11b075680502e625debc2f
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-25 08:43:46 +0300

    Add task_2

--- Practice_25/Practice_25.Task_2/MainWindow.xaml.cs
+++ Practice_25/Practice_25.Task_2/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Practice_25.Task_2
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
