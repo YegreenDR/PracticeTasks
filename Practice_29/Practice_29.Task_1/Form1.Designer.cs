fe7537b592dcb478fe0414d3d98769a09b5cde9b
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-29 13:59:27 +0300

    Add task_1

--- Practice_29/Practice_29.Task_1/Form1.Designer.cs
+++ Practice_29/Practice_29.Task_1/Form1.Designer.cs
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStartTimer = new System.Windows.Forms.Button();
+            this.btnStopTimer = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            // 
+            // btnStartTimer
+            // 
+            this.btnStartTimer.Location = new System.Drawing.Point(65, 321);
+            this.btnStartTimer.Name = "btnStartTimer";
+            this.btnStartTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnStartTimer.TabIndex = 0;
+            this.btnStartTimer.Text = "Start";
+            this.btnStartTimer.UseVisualStyleBackColor = true;
+            this.btnStartTimer.Click += new System.EventHandler(this.btnStartTimer_Click);
+            // 
+            // btnStopTimer
+            // 
+            this.btnStopTimer.Location = new System.Drawing.Point(175, 321);
+            this.btnStopTimer.Name = "btnStopTimer";
+            this.btnStopTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnStopTimer.TabIndex = 0;
+            this.btnStopTimer.Text = "Stop";
+            this.btnStopTimer.UseVisualStyleBackColor = true;
+            this.btnStopTimer.Click += new System.EventHandler(this.btnStopTimer_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(774, 342);
+            this.ClientSize = new System.Drawing.Size(774, 378);
+            this.Controls.Add(this.btnStopTimer);
+            this.Controls.Add(this.btnStartTimer);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
@@ -43,6 +74,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStartTimer;
+        private System.Windows.Forms.Button btnStopTimer;
     }
 }
 
--- Practice_29/Practice_29.Task_1/Form1.cs
+++ Practice_29/Practice_29.Task_1/Form1.cs
@@ -6,33 +6,50 @@ namespace Practice_29.Task_1
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        private Pen pen;
         public Form1()
         {
             InitializeComponent();
+            pen = new Pen(Color.Black, 2);
+            this.Load += Form1_Load;
+            timer1.Tick += timer1_Tick;
+            btnStartTimer.Click += btnStartTimer_Click;
+            btnStopTimer.Click += btnStopTimer_Click;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            r = 150;
+            a = 0;
+            x1 = this.ClientSize.Width / 2;
+            y1 = this.ClientSize.Width / 2;
+            x2 = x1 + (int)(r * Math.Cos(a));
+            y2 = y1 - (int)(r * Math.Sin(a));
+        }
+        private void btnStopTimer_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void btnStartTimer_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
         }
         private int x1, y1, x2, y2, r;
+        private double a;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            g.DrawLine(pen, x1, y1, x2, y2);
+        }
         private void timer1_Tick(object sender, EventArgs e)
         {
             a -= 0.1;
             x2 = x1 + (int)(r * Math.Cos(a));
             y2 = y1 - (int)(r * Math.Sin(a));
             Invalidate();
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            r = 150;
-            a = 0;
-            x1 = ClientSize.Width / 2;
-            y1 = ClientSize.Width / 2;
-            x2 = x1 + (int)(r * Math.Cos(a));
-            y2 = y1 - (int)(r * Math.Sin(a));
-        }
-        private double a;
-        private Pen pen = new Pen(Color.Black, 2);
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-            g.DrawLine(pen, x1, y1, x2, y2);
         }
     }
 }
