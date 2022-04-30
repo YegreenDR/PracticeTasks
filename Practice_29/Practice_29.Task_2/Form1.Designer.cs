5c0e11b633e74aa01c6a898e93877bcf5355c48f
Влад Эйсмонт <eysmont73@gmail.com>
2022-04-30 09:25:39 +0300

    Add task_2

--- Practice_29/Practice_29.Task_2/Form1.Designer.cs
+++ Practice_29/Practice_29.Task_2/Form1.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(801, 317);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Practice_29/Practice_29.Task_2/Form1.cs
+++ Practice_29/Practice_29.Task_2/Form1.cs
@@ -12,14 +12,58 @@ namespace Practice_29.Task_2
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Bitmap baner;
+        Rectangle rct;
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                baner = new Bitmap(@"E:\Практика\C#\Practice\PracticeTasks\Practice_29\Practice_29.Task_2\baner.png");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Ошибка загрузки файла баннера\n" + e.ToString(), "Баннер");
+                this.Close();
+                return;
+            }
+            this.Paint += Form1_Paint;
+
+            rct.X = 0;
+            rct.Y = 0;
+            rct.Width = baner.Width;
+            rct.Height = baner.Height;
+            timer1.Interval = 50;
+            timer1.Enabled = true;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            g = e.Graphics;
+            g.DrawImage(baner, rct.X + rct.Width, rct.Y);
+        }
 
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            rct.X -= 4;
+            if (Math.Abs(rct.X) > rct.Width && rct.X < 0)
+                rct.X = this.ClientSize.Width + 2;
+            Invalidate();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Y < rct.Y + rct.Height) && (e.Y > rct.Y))
+            {
+                if (timer1.Enabled != true)
+                    timer1.Enabled = false;
+            }
+            else
+            {
+                if (timer1.Enabled != true)
+                    timer1.Enabled = true;
+            }
         }
     }
 }
